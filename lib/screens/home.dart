fe19747fcb207caeda305cdaa5c28a38ea66d99f
raviteja <iec2018045@gmail.com>
2020-06-03 01:00:39 +0530

    app bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'package:covidtracker/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -5,12 +6,30 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: SvgPicture.asset("assets/icons/menu.svg"),
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: kPrimaryColor.withOpacity(0.03),
+      elevation: 0,
+      leading: IconButton(
+        icon: Icon(
+          Icons.menu,
+          color: kPrimaryColor,
+        ),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: kPrimaryColor,
+          ),
           onPressed: () {},
         ),
-      ),
+      ],
     );
   }
 }
