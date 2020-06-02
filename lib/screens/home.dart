8411305e517d108d22848c9a40101c4e856345b2
raviteja <iec2018045@gmail.com>
2020-06-03 00:12:22 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(leading: IconButton(icon: Icon(Icons.menu), onPressed: null),),
       
     );
   }
