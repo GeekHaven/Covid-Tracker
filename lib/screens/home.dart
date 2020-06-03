18c28a2f6362c2e605fd5f4fd1aff661fb6f0123
raviteja <iec2018045@gmail.com>
2020-06-03 14:25:27 +0530

    all cards

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,88 +1,94 @@
 import 'package:covidtracker/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:fl_chart/fl_chart.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(),
-      body: Container(
-        height: 250,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          color: kPrimaryColor.withOpacity(0.03),
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(50),
-            bottomRight: Radius.circular(50),
-          ),
-        ),
-        child: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2, childAspectRatio: 300 / 200),
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                child: Column(
-                  children: <Widget>[
-                    Row(
+    var padding2 = Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Column(
                       children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.all(5),
-                          alignment: Alignment.center,
-                          height: 30,
-                          width: 30,
-                          decoration: BoxDecoration(
-                              color: kPrimaryColor.withOpacity(0.03),
-                              shape: BoxShape.circle),
-                          child: FittedBox(child: Icon(Icons.people)),
+                        Row(
+                          children: <Widget>[
+                            Container(
+                              margin: EdgeInsets.all(5),
+                              alignment: Alignment.center,
+                              height: 30,
+                              width: 30,
+                              decoration: BoxDecoration(
+                                  color: kPrimaryColor.withOpacity(0.03),
+                                  shape: BoxShape.circle),
+                              child: FittedBox(child: Icon(Icons.people)),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              'Total cases',
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
                         ),
-                        SizedBox(
-                          width: 10,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: <Widget>[
+                              RichText(
+                                text: TextSpan(
+                                  style: TextStyle(color: kTextColor),
+                                  children: [
+                                    TextSpan(
+                                      text: '1062\n',
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline6
+                                          .copyWith(
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                    TextSpan(
+                                      text: 'people',
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              
+                            ],
+                          ),
                         ),
-                        Text(
-                          'Total cases',
-                          maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                        )
                       ],
                     ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: <Widget>[
-                          RichText(
-                            text: TextSpan(
-                              style: TextStyle(color: kTextColor),
-                              children: [
-                                TextSpan(
-                                  text: '1062\n',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline6
-                                      .copyWith(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                ),
-                                TextSpan(
-                                  text: 'people',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                          
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                elevation: 5,
+                    elevation: 5,
+                  ),
+                  
+                );
+        return Scaffold(
+          appBar: buildAppBar(),
+          body: Container(
+            height: 240,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: kPrimaryColor.withOpacity(0.03),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
               ),
             ),
+            child: GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 300 / 180),
+              children: <Widget>[
+                padding2,
+                padding2,
+                padding2,
+                padding2,
+            
           ],
         ),
       ),
