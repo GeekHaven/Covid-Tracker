f4b2d1d025dbdbaaedf759bdc57e4967195a261a
raviteja <iec2018045@gmail.com>
2020-06-03 14:12:53 +0530

    cards updated

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,14 @@
 import 'package:covidtracker/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:fl_chart/fl_chart.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
       body: Container(
-        height: 300,
+        height: 250,
         width: double.infinity,
         decoration: BoxDecoration(
           color: kPrimaryColor.withOpacity(0.03),
@@ -18,16 +18,72 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
         child: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          children: List.generate(4, (index) {
-            return Card(
-              elevation: 10,
-              child: Text(
-                'Item $index',
-              
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, childAspectRatio: 300 / 200),
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.all(5),
+                          alignment: Alignment.center,
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                              color: kPrimaryColor.withOpacity(0.03),
+                              shape: BoxShape.circle),
+                          child: FittedBox(child: Icon(Icons.people)),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Total cases',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: <Widget>[
+                          RichText(
+                            text: TextSpan(
+                              style: TextStyle(color: kTextColor),
+                              children: [
+                                TextSpan(
+                                  text: '1062\n',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline6
+                                      .copyWith(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                ),
+                                TextSpan(
+                                  text: 'people',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                elevation: 5,
               ),
-            );
-          }),
+            ),
+          ],
         ),
       ),
     );
