86e603eacbc9b529e47efe52f290a21cddcfe53f
raviteja <iec2018045@gmail.com>
2020-06-03 14:50:10 +0530

    widgets folder

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:covidtracker/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import './widgets/Infocard.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -56,79 +57,3 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class Infocard extends StatelessWidget {
- final String title;
- final int effectedNum;
- final Color iconcolor;
- 
-  const Infocard({
-    Key key, this.title, this.effectedNum, this.iconcolor,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(5),
-                  alignment: Alignment.center,
-                  height: 30,
-                  width: 30,
-                  decoration: BoxDecoration(
-                      color: iconcolor.withOpacity(0.3),
-                      shape: BoxShape.circle),
-                  child: FittedBox(child: Icon(Icons.people)),
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                )
-              ],
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  RichText(
-                    text: TextSpan(
-                      style: TextStyle(color: kTextColor),
-                      children: [
-                        TextSpan(
-                          text: '$effectedNum\n',
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline6
-                              .copyWith(
-                                fontWeight: FontWeight.bold,
-                              ),
-                        ),
-                        TextSpan(
-                          text: 'people',
-                          style: TextStyle(
-                            fontSize: 12,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  
-                ],
-              ),
-            ),
-          ],
-        ),
-        elevation: 5,
-      ),
-      
-    );
-  }
-}
