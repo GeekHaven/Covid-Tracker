8af9a5e7f97230ade16f5c0c8c8219a3e8377a54
rohithmone27 <rohithmone27@gamil.com>
2020-06-09 01:16:00 +0530

    Nothing Updated

--- lib/screens/home/indiascreen/showdistrictdata.dart
+++ lib/screens/home/indiascreen/showdistrictdata.dart
@@ -1,16 +1,8 @@
-import 'dart:io';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
-// Sets a platform override for desktop to avoid exceptions. See
-// https://flutter.dev/desktop#target-platform-override for more info.
-// void enablePlatformOverrideForDesktop() {
-//   if (!kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux)) {
-//     debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-//   }
-// }
+
 
 class ShowDistrictData extends StatefulWidget {
   var districtData;
