b8e142c094532f7a8493a2720dd9ab7319e0cb7c
rohithmone27 <rohithmone27@gamil.com>
2020-06-14 17:49:19 +0530

    NOne

--- lib/screens/home/home_widgets/newsscreen.dart
+++ lib/screens/home/home_widgets/newsscreen.dart
@@ -7,52 +7,63 @@ class NewsScreen extends StatelessWidget {
   var textSpan = TextSpan(
       text: ' News\n',
       style: TextStyle(
-          fontWeight: FontWeight.bold, color: Colors.green[700], fontSize: 1.96 * SizeConfig.textMultiplier));
+          fontWeight: FontWeight.bold,
+          color: Colors.green[700],
+          fontSize: 1.96 * SizeConfig.textMultiplier));
   @override
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(3.27 * SizeConfig.heightMultiplier),
+          borderRadius:
+              BorderRadius.circular(3.27 * SizeConfig.heightMultiplier),
         ),
         child: Row(
           children: <Widget>[
-            FlatButton(
-              textColor: Colors.green[700],
-              padding: EdgeInsets.only(top: 1.1 * SizeConfig.heightMultiplier, left: 4.73 * SizeConfig.widthMultiplier),
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return NewsView(false);
-                }));
-              },
-              child: Center(
-                child: Card(
-                  elevation: 8,
-                  child: Container(
-                    padding: EdgeInsets.all(0.85 * SizeConfig.heightMultiplier),
-                    child: Row(
-                      children: <Widget>[
-                        Text(
-                          "Corona ",
-                          style: TextStyle(
-                              fontSize: 2.4 * SizeConfig.textMultiplier, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          "News ",
-                          style: TextStyle(
-                              fontSize: 2.4 * SizeConfig.textMultiplier,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.green[700]),
-                        ),
-                      ],
+              FlatButton(
+                textColor: Colors.green[700],
+                padding: EdgeInsets.only(
+                    top: 1.1 * SizeConfig.heightMultiplier,
+                    left: 4.73 * SizeConfig.widthMultiplier),
+                onPressed: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return NewsView(false);
+                  }));
+                },
+                child: Center(
+                  child: Card(
+                    elevation: 8,
+                    child: Container(
+                      padding:
+                          EdgeInsets.all(0.85 * SizeConfig.heightMultiplier),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "Corona ",
+                            style: TextStyle(
+                                fontSize: 2.4 * SizeConfig.textMultiplier,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            "News ",
+                            style: TextStyle(
+                                fontSize: 2.4 * SizeConfig.textMultiplier,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.green[700]),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: 57 * SizeConfig.widthMultiplier,
-              child: SvgPicture.asset('assets/images/updates.svg'),
+            
+            Expanded(
+              flex: 1,
+              child: SizedBox(
+                width: 57 * SizeConfig.widthMultiplier,
+                child: SvgPicture.asset('assets/images/updates.svg'),
+              ),
             ),
           ],
         ));
